f607825b051de098e7c53a7a2342c0a22c49db19
Kia Boluki <Kia.Boluki@gmail.com>
2021-08-12 17:05:29 +0430

    bs5 install, desktop create, desktop bg assigned

---
+++ src/components/desktop.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Desktop extends Component {
+    state = {  }
+    render() { 
+        return ( <div className="desktop">
+            
+        </div> );
+    }
+}
+ 
+export default Desktop;
